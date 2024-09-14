f3f534eebddfbe00385323c1e2964bd3230af84c
Jason Meng <sysbender@gmail.com>
2024-09-14 18:10:27 -0400

    child locator

--- tests/locator.spec.ts
+++ tests/locator.spec.ts
@@ -29,10 +29,10 @@ test('locator syntax', async ({page})=>{
     
 })
 
-test.only("visible locator", async ({page})=>{
+test("visible locator", async ({page})=>{
     await page.getByRole('textbox', {name:'Email'}).first().click();
     await page.getByRole('button', {name: 'Sign in'}).first().click();
-    
+
     await page.getByLabel('Email').first().click();
     await page.getByPlaceholder('Jane Doe').click();
     await page.getByText('Using the Grid').click();
@@ -43,4 +43,12 @@ test.only("visible locator", async ({page})=>{
 
 
 
+})
+
+test.only( ' child locator', async ({page}) =>{
+    await page.locator('nb-card nb-radio :text-is("Option 1")').click()
+    await page.locator('nb-card').locator('nb-radio').locator(':text-is("Option 2")').click()
+
+
+    await page.locator('nb-card').nth(3).getByRole('button').click()
 })
