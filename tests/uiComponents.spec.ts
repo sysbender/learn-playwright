31b23e2010c37421749da0fd47dd6f3aafdc37d1
Jason Meng <sysbender@gmail.com>
2024-09-16 19:55:39 -0400

    list and dropdown

--- tests/uiComponents.spec.ts
+++ tests/uiComponents.spec.ts
@@ -1,4 +1,5 @@
 import {test, expect} from '@playwright/test'
+import exp from 'constants'
 
 
 // go to homepage
@@ -84,4 +85,48 @@ test.describe('Form Model page' , ()=>{
         }
     } )
 
+
+
 })
+
+ 
+    test('list and dropdown' , async ({page})=>{
+        const menu = page.locator('ngx-header nb-select')
+        await menu.click()
+
+        // getByRole('list') - if ul
+        // getByRole('listitem') - if li
+
+        const listElem = page.getByRole('list')
+        //const listItemElements = listElem.locator('nb-option')
+
+        const listItemElements = page.locator('nb-option-list nb-option')
+        await expect(listItemElements).toHaveText(['Light', 'Dark', "Cosmic", "Corporate"])
+        await listItemElements.filter({hasText: 'Cosmic'}).click()
+
+      
+        const header = page.locator('nb-layout-header')
+        await expect(header).toHaveCSS('background-color' , 'rgb(50, 50, 89)')
+
+
+        const colors = {
+            "Light": "rgb(255, 255, 255)",
+            "Dark": "rgb(34, 43, 69)",
+            "Cosmic": "rgb(50, 50, 89)",
+            "Corporate": "rgb(255, 255, 255)",
+        }
+
+
+        for (const color in colors){
+            // click list
+            await menu.click()
+
+            // click item 
+            await listItemElements.filter({hasText: color}).click()
+
+            // check color
+            await expect(header).toHaveCSS('background-color', colors[color])
+        }
+
+    })
+ 
