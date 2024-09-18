355bdcd91b8c73b151acf326b63b54a7384d03aa
Jason Meng <sysbender@gmail.com>
2024-09-18 19:51:27 -0400

    page object 1

---
+++ tests/usePageObjects.spec.ts
@@ -0,0 +1,12 @@
+import {test, expect} from '@playwright/test'
+import {NavigationPage} from '../page-objects/navigationPage.ts'
+
+test.beforeEach( async ({page})=>{
+    await page.goto('http://192.168.0.142:4200')
+})
+
+
+test('navigate to form page', async ({page})=>{
+    const navigateTo  = new  NavigationPage(page)
+    await navigateTo.formLayoutsPage()
+})
