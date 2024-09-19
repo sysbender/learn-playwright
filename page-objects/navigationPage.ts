9299f13c31e4548d8b4975d6f0efa270421cb9ce
Jason Meng <sysbender@gmail.com>
2024-09-18 20:25:12 -0400

    navigate to page

--- page-objects/navigationPage.ts
+++ page-objects/navigationPage.ts
@@ -1,15 +1,38 @@
-import {Page} from '@playwright/test'
+import { Page } from "@playwright/test";
 
 export class NavigationPage {
+  readonly page: Page;
+  constructor(page: Page) {
+    this.page = page;
+  }
 
+  async formLayoutsPage() {
+    await this.click2Text("Forms", "Form Layouts");
+  }
 
-    readonly page: Page;
-    constructor(page:Page){
-        this.page = page
+  async datepickerPage() {
+    await this.click2Text("Forms", "Datepicker");
+  }
+
+  async smartTablePage() {
+    await this.click2Text("Tables & Data", "Smart Table");
+  }
+
+  async toastrPage() {
+    await this.click2Text("Modal & Overlays", "Toastr");
+  }
+
+  async tooltipPage() {
+    await this.click2Text("Modal & Overlays", "Tooltip");
+  }
+
+  private async click2Text(text1: string, text2: string) {
+    const elem1 = this.page.getByText(text1);
+    const expandAttr = "aria-expanded";
+    const isExpand = await elem1.getAttribute(expandAttr);
+    if (!isExpand) {
+      await this.page.getByText(text1).click();
     }
-
-    async formLayoutsPage(){
-        await this.page.getByText('Forms').click()
-        await this.page.getByText('Form Layouts').click()
-    }
+    await this.page.getByText(text2).click();
+  }
 }
--- tests/usePageObjects.spec.ts
+++ tests/usePageObjects.spec.ts
@@ -1,12 +1,15 @@
-import {test, expect} from '@playwright/test'
-import {NavigationPage} from '../page-objects/navigationPage.ts'
+import { test, expect } from "@playwright/test";
+import { NavigationPage } from "../page-objects/navigationPage.ts";
 
-test.beforeEach( async ({page})=>{
-    await page.goto('http://192.168.0.142:4200')
-})
+test.beforeEach(async ({ page }) => {
+  await page.goto("http://192.168.0.142:4200");
+});
 
-
-test('navigate to form page', async ({page})=>{
-    const navigateTo  = new  NavigationPage(page)
-    await navigateTo.formLayoutsPage()
-})
+test("navigate to form page", async ({ page }) => {
+  const navigateTo = new NavigationPage(page);
+  await navigateTo.formLayoutsPage();
+  await navigateTo.datepickerPage();
+  await navigateTo.smartTablePage();
+  await navigateTo.toastrPage();
+  await navigateTo.tooltipPage();
+});
